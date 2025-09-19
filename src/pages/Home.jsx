b90c29ebe7f960260ee10c67961c564bff582419
semonemo00 <rsemo@naver.com>
2025-09-19 09:23:08 +0900

    페이지 이동

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   // logic
+  const history= useNavigate()
 
   const handleStart = () => {
-    console.log("info페이지로 이동");
+    history("/info")
   };
 
   // view
   return (
     <div className="w-full h-full px-6 pt-10 break-keep overflow-auto">
       <i className="w-168 h-168 rounded-full bg-chef-green-500 fixed -z-10 -left-60 -top-96"></i>
-      <div className="fixed left-0 top-1/2 transform -translate-y-1/3 -z-10">
+      <div className="fixed left-0 top-1/2 transfrm -translate-y-1/3 -z-10">
         <img src="./images/hero.svg" alt="hero" />
       </div>
       <div className="h-full flex flex-col">
